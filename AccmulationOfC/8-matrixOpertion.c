e87fcadf57d4fdd9a7180b56fdcdcc21f527dee1
lifecrazy <hengzhang0630@126.com>
2014-12-04 19:43:40 +0800

    关于矩阵的几个操作

--- AccmulationOfC/8-matrixOpertion.c
+++ AccmulationOfC/8-matrixOpertion.c
@@ -7,7 +7,11 @@
 //矩阵的查找
 //题目：假设矩阵从上到下依次增大从左到右依次增大，查找每个数字是否在这个矩阵当中
 //如果要查找的元素存在则返回true(1)否则返回false(0)
-//思路：
+//思路：从右上角开始查找
+//                       如果Arr[i][j] > key即key小于右上角的那个元素那么key就一定在左边的列当中所以行递减
+//                       如果Arr[i][j] > key即key小于右上角的那个元素那么key就一定在下边的行当中所以列递增
+//                       如果Arr[i][j] = key即为找到
+//也可以从左下角开始查找思路与上边一样
 int matrixSearchKey(int **Arr, int num, int key, int *result)
 {
 	int ret = 0;
@@ -39,6 +43,7 @@ int matrixSearchKey(int **Arr, int num, int key, int *result)
 	return ret;
 }
 //题目：将矩阵旋转九十度
+//思路：将整个矩阵先按照副对角线翻转，然后按照中轴线上下翻转就能使其旋转九十度
 int matrixRotating(int **Arr, int num)
 {
 	int ret = 0;
@@ -71,7 +76,8 @@ int matrixRotating(int **Arr, int num)
 	}
 	return ret;
 }
-//转置
+//题目：矩阵转置
+//将整个矩阵按照主对角线翻转就能得到结果
 int matrixTransposition(int **Arr, int num)
 {
 	int ret = 0;
@@ -109,8 +115,10 @@ void main()
 	int result;
 	matrixSearchKey(a, n, 9, &result);
 	printf("查找结果：%d\n", result);
-	//matrixRotating(a, n);
-	//matrixPrint(a, n);
+	printf("矩阵旋转九十度：\n");
+	matrixRotating(a, n);
+	matrixPrint(a, n);
+	printf("矩阵转置：\n");
 	matrixTransposition(a, n);
 	matrixPrint(a, n);
 	system("pause");
