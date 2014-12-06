a8740a99a1414c21fa36619d16a336ac6efd1d6a
lifecrazy <hengzhang0630@126.com>
2014-12-06 18:59:36 +0800

    几种排序算法

--- AccmulationOfC/1-sortList.c
+++ AccmulationOfC/1-sortList.c
@@ -14,6 +14,8 @@
 6.归并排序
 7.基数排序
 */
+
+#pragma region 打印数组
 //打印数组
 int printArr(int *Arr, int num)
 {
@@ -30,6 +32,9 @@ int printArr(int *Arr, int num)
 	}
 	return ret;
 }
+#pragma endregion
+
+#pragma region 交换数据元素
 //交换数据元素
 int swapNum(int *a, int *b)
 {
@@ -45,6 +50,10 @@ int swapNum(int *a, int *b)
 	*b = temp;
 	return ret;
 }
+#pragma endregion
+
+
+#pragma region 1.插入排序
 //1.插入排序
 //思路：已知数据元素a[0]到a[n-1]，假设前k（0 < k <=n-1）个元素从小到大有序，然后让第k+1个元素与前k个元素依次倒着比较知道找到第一个比第k+1个元素小的元素，然后将其插入这个元素的后面即可，直到k = n-1;
 int insertSort(int *Arr, int num)
@@ -70,6 +79,10 @@ int insertSort(int *Arr, int num)
 	}
 	return ret;
 }
+#pragma endregion
+
+
+#pragma region 2.冒泡排序
 //2.冒泡排序
 //思路：每次选择一个最大的的元素将其放置在比较的数组元素的最后
 int bubbleSort(int *Arr, int num)
@@ -101,6 +114,10 @@ int bubbleSort(int *Arr, int num)
 	}
 	return ret;
 }
+#pragma endregion
+
+
+#pragma region 3.快速排序
 //3.快速排序
 //思路：每次选择一个元素比它大的元素放在它右边比它小的元素放在它左边
 int partition(int *Arr, int left, int right, int*index)
@@ -158,6 +175,10 @@ int quickSort(int *Arr, int left, int right)
 	}
 	return ret;
 }
+#pragma endregion
+
+
+#pragma region 4.选择排序
 //我犯过的错误就是弄错了数组数据类型和数据元素数据类型
 //4.选择排序
 //思路：假设前k（0 < k <=n-1）个元素从小到大有序，每次都从剩下的n-1-k个元素中选择一个元素加入到前k个元素中使其k+1个元素有序。
@@ -182,6 +203,10 @@ int SelectSort(int *Arr, int num)
 	}
 	return ret;
 }
+#pragma endregion
+
+
+#pragma region 5.堆排序（大根堆）
 //5.堆排序（大根堆）
 //思路：首先是创建堆，然后是排序
 //调整堆
@@ -245,6 +270,10 @@ int heapSort(int *Arr, int num)
 	}
 	return ret;
 }
+#pragma endregion
+
+
+#pragma region 6.归并排序
 //6.归并排序
 //思路：先划分，然后再合并
 int mergeSortCore(int *Arr, int first, int mid, const int last)
@@ -306,7 +335,9 @@ int mergeSort(int *Arr, int first, int last)
 	}
 	return ret;
 }
+#pragma endregion
 
+#pragma region 7.基数排序
 //7.基数排序
 int getNumOfPos(int num, int pos)
 {
@@ -337,7 +368,7 @@ int msdradixSort(int *Arr, int num)
 	}
 	for (int pos = 1; pos <= n; pos++)    //从个位开始到n位  
 	{
-		for (int i = 0; i < num; i++)     
+		for (int i = 0; i < num; i++)
 		{
 			//得到每个数组元素对应的位的数字
 			int posNum = getNumOfPos(Arr[i], pos);
@@ -358,6 +389,9 @@ int msdradixSort(int *Arr, int num)
 	}
 	return ret;
 }
+#pragma endregion
+
+
 void mainforsortlist()
 {
 	int num = 10;
