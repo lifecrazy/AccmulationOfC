77cf9fbd9c3de7b5946c418449db114a1e8a1918
lifecrazy <hengzhang0630@126.com>
2014-12-06 19:34:10 +0800

    矩阵的几种打印方法

--- AccmulationOfC/6-matrixPrint.c
+++ AccmulationOfC/6-matrixPrint.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#pragma region 1.矩阵的初始化方法一
 //1.矩阵的初始化方法一
 int matrixInit(int **Arr, int num)
 {
@@ -15,13 +16,16 @@ int matrixInit(int **Arr, int num)
 		printf("矩阵初始化失败！！！");
 		return ret;
 	}
-	
+
 	for (int i = 0; i < num*num; i++)
 	{
 		Arr[i / num][i%num] = i;
 	}
 	return ret;
 }
+#pragma endregion
+
+#pragma region 2.矩阵的初始化方法二
 //2.矩阵的初始化方法二
 int matrixInitOfSecond(int **Arr, int num)
 {
@@ -41,6 +45,10 @@ int matrixInitOfSecond(int **Arr, int num)
 	}
 	return ret;
 }
+#pragma endregion
+
+
+#pragma region 3.打印矩阵
 //3.打印矩阵
 int matrixPrint(int **Arr, int num)
 {
@@ -59,9 +67,13 @@ int matrixPrint(int **Arr, int num)
 		}
 		printf("\n");
 	}
-	
+
 	return ret;
 }
+#pragma endregion
+
+
+#pragma region 4.按照回型打印
 //4.按照回型打印
 int matrixPrintByEdge(int **Arr, int num)
 {
@@ -72,29 +84,33 @@ int matrixPrintByEdge(int **Arr, int num)
 		printf("矩阵初始化失败！！！");
 		return ret;
 	}
-	for (int i = 0; i <num; i++)//列
+	for (int i = 0; i < num; i++)//列
 	{
-		for (int j = i; j <num-i; j++)//行
+		for (int j = i; j < num - i; j++)//行
 		{
 			printf("%d  ", Arr[i][j]);
 		}
-		for (int j = i+1; j < num-i; j++)
+		for (int j = i + 1; j < num - i; j++)
 		{
-			printf("%d  ", Arr[j][num-i-1]);
+			printf("%d  ", Arr[j][num - i - 1]);
 		}
-		for (int j = num-i-2; j >=i; j--)
+		for (int j = num - i - 2; j >= i; j--)
 		{
 			printf("%d  ", Arr[num - i - 1][j]);
 		}
-		for (int j = num - i - 2; j >i; j--)
+		for (int j = num - i - 2; j > i; j--)
 		{
 			printf("%d  ", Arr[j][i]);
 		}
 	}
 	printf("\n");
-	
+
 	return ret;
 }
+#pragma endregion
+
+
+#pragma region 5.按照主对角线从上往下打印
 //5.按照主对角线从上往下打印
 int matrixPrintByDiagonalFirst(int **Arr, int num)
 {
@@ -121,10 +137,14 @@ int matrixPrintByDiagonalFirst(int **Arr, int num)
 		}
 		printf("\n");
 	}
-	
+
 
 	return ret;
 }
+#pragma endregion
+
+
+#pragma region 6.按照主对角线从下往上打印
 //6.按照主对角线从下往上打印
 int matrixPrintByDiagonalSec(int **Arr, int num)
 {
@@ -153,6 +173,10 @@ int matrixPrintByDiagonalSec(int **Arr, int num)
 	}
 	return ret;
 }
+#pragma endregion
+
+
+#pragma region 7.按照副对角线从上往下打印
 //7.按照副对角线从上往下打印
 int matrixPrintByDiagonalThird(int **Arr, int num)
 {
@@ -181,6 +205,10 @@ int matrixPrintByDiagonalThird(int **Arr, int num)
 	}
 	return ret;
 }
+#pragma endregion
+
+
+#pragma region 8.按照副对角线从下往上打印
 //8.按照副对角线从下往上打印
 int matrixPrintByDiagonalForth(int **Arr, int num)
 {
@@ -191,24 +219,27 @@ int matrixPrintByDiagonalForth(int **Arr, int num)
 		printf("矩阵初始化失败！！！");
 		return ret;
 	}
-	for (int i =0; i <num; i++)//列
+	for (int i = 0; i < num; i++)//列
 	{
-		for (int j = i; j >=0; j--)//行
+		for (int j = i; j >= 0; j--)//行
 		{
-			printf("%d  ", Arr[j][num-1-(i-j)]);
+			printf("%d  ", Arr[j][num - 1 - (i - j)]);
 		}
 		printf("\n");
 	}
 	for (int i = 1; i < num; i++)//行
 	{
-		for (int j = num-1-i; j >=0; j--)//列
+		for (int j = num - 1 - i; j >= 0; j--)//列
 		{
-			printf("%d  ", Arr[i+j][j]);
+			printf("%d  ", Arr[i + j][j]);
 		}
 		printf("\n");
 	}
 	return ret;
 }
+#pragma endregion
+
+
 void mainformatrixPrint()
 {
 	const int n = 4;
