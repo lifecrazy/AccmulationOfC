bca6bc3a1c645be2c9422d434a0c6ad733a7f91f
lifecrazy <hengzhang0630@126.com>
2014-12-06 19:03:28 +0800

    进制转换

--- AccmulationOfC/2-convert.c
+++ AccmulationOfC/2-convert.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#pragma region 十进制转二进制
 int ConvertTo2(int num, char* result)
 {
 	int ret = 0;
@@ -35,6 +36,10 @@ int ConvertTo2(int num, char* result)
 	}
 	return ret;
 }
+#pragma endregion
+
+
+#pragma region 十进制转八进制
 int ConvertTo8(int num, char* result)
 {
 	int ret = 0;
@@ -64,6 +69,10 @@ int ConvertTo8(int num, char* result)
 	}
 	return ret;
 }
+#pragma endregion
+
+
+#pragma region 十进制转十六进制
 int ConvertTo16(int num, char* result)
 {
 	int ret = 0;
@@ -122,6 +131,10 @@ int ConvertTo16(int num, char* result)
 	}
 	return ret;
 }
+#pragma endregion
+
+
+#pragma region 二进制转十进制
 int convertFrom2(char *num, int* result)
 {
 	int ret = 0;
@@ -143,6 +156,10 @@ int convertFrom2(char *num, int* result)
 	*result = temp;
 	return ret;
 }
+#pragma endregion
+
+
+#pragma region 八进制转十进制
 int convertFrom8(char *num, int* result)
 {
 	int ret = 0;
@@ -164,6 +181,10 @@ int convertFrom8(char *num, int* result)
 	*result = temp;
 	return ret;
 }
+#pragma endregion
+
+
+#pragma region 十六进制转十进制
 int convertFrom16(char *num, int* result)
 {
 	int ret = 0;
@@ -212,6 +233,9 @@ int convertFrom16(char *num, int* result)
 	*result = temp;
 	return ret;
 }
+#pragma endregion
+
+
 void mainforconvert()
 {
 	int num = 34;
