17ec687cd0117b6b923021fd920bcfe0b66be018
lifecrazy <hengzhang0630@126.com>
2014-12-09 09:58:04 +0800

    字符串的几种操作（自己实现）

--- AccmulationOfC/7-stringOperation.c
+++ AccmulationOfC/7-stringOperation.c
@@ -6,7 +6,9 @@
 //strcpy字符串拷贝。
 //strstr查找子字符串。
 //strtok字符串按照某个字符分割。
+#pragma region 1.strlen
 //1.strlen
+//思路：定义一个临时指针在字符串中查找，找到\0即为结束。
 int mystrLen(char *str, int *count)
 {
 	int ret = 0;
@@ -26,7 +28,11 @@ int mystrLen(char *str, int *count)
 	*count = result;
 	return ret;
 }
+#pragma endregion
+
+#pragma region 2.strcpy
 //2.strcpy
+//思路：定义一个指针指向要拷贝的地址然后依次拷贝要拷贝的字符串直到\0
 int mystrcpy(char *strDest, char *strSrc)
 {
 	int ret = 0;
@@ -43,7 +49,13 @@ int mystrcpy(char *strDest, char *strSrc)
 	}
 	return ret;
 }
+#pragma endregion
+
+
+#pragma region 3.strcat
 //3.strcat
+//思路：先获取追加字符串的末尾然后定义一个指针指向其末尾然后依次拷贝要追加的字符串直到\0为止
+//与strcpy差不多只是拷贝字符的起点不一样
 int mystrcat(char *strDest, char *strSrc)
 {
 	int ret = 0;
@@ -61,7 +73,12 @@ int mystrcat(char *strDest, char *strSrc)
 	}
 	return ret;
 }
+#pragma endregion
+
+
+#pragma region 4.strstr
 //4.strstr
+//思路：就是回溯法，在haystack中依次查找needle 如果查到则返回在haystack中的index,如果没有找到则让在haystack中查找的指针回溯到和needle 一起开始查找的位置的下一个字符继续开始和needle一起查找。
 int mystrstr(char *haystack, char *needle, int *index)
 {
 	int ret = 0;
@@ -98,8 +115,15 @@ int mystrstr(char *haystack, char *needle, int *index)
 	*index = -1;
 	return ret;
 }
+#pragma endregion
 
+
+
+#pragma region 5.strtok
 //5.strtok
+//思路：1.由于不知道字符串str1会被分割成多少个子字符串，那么先申请两个数组指针，当分割后的字符串超过了分配的数组指针的容量时，就翻倍申请四个数组指针将之前的两个数组指针拷贝过来然后释放掉之前申请的内存空间，按照这个思路依次下去。
+//      2.定义两个指针pCur，pTemp指向str1的首地址，让一个指针pCur去查找分割的字符，找到之后分配pCur - pTemp+1个内存，多分配一个是为了存放\0然后将该子字符串拷贝到申请的内存空间中再将该内存空间挂到之前申请的数组指针当中。
+//      3.当所有的分隔符都找到之后还剩下的字符串作为一个字符数组单独处理一下
 int mystrtok(char *str1, const char *str2, char ***buf, int *num)
 {
 	int ret = 0;
@@ -135,7 +159,7 @@ int mystrtok(char *str1, const char *str2, char ***buf, int *num)
 				char **pResult = (char **)malloc(capacity*sizeof(char*));
 				for (int i = 0; i < capacity / 2; i++)
 				{
-					 pResult[i] = result[i];
+					pResult[i] = result[i];
 				}
 				free(result);
 				result = pResult;
@@ -189,8 +213,13 @@ void freeChar(char **Arr, int num)
 	}
 	free(Arr);
 }
+#pragma endregion
 
+
+
+#pragma region 7.strrev
 //7.strrev
+//思路：定义两个指针分别指向开始和结尾，依次交换彼此的内容直到两者相遇为止。
 int reversalStr(char *str)
 {
 	int ret = 0;
@@ -213,29 +242,11 @@ int reversalStr(char *str)
 	}
 	return ret;
 }
+#pragma endregion
 
-int trimSpace(char *str, char *outbuf)
-{
-	int ret = 0;
-	if (str == NULL || outbuf == NULL)
-	{
-		ret = -1;
-		printf("func trimSpace err :%d", ret);
-		return ret;
-	}
-	char *start = str;
-	char *end = str + strlen(str) - 1;
-	while (start < end&&*start == ' ')
-	{
-		start++;
-	}
-	while (start < end&&*end == ' ')
-	{
-		end--;
-	}
-	memcpy(outbuf, start, end - start + 1);
-	outbuf[end - start + 1] = '\0';
-}
+
+
+
 void main()
 {
 	char *p = "abcdefg";
